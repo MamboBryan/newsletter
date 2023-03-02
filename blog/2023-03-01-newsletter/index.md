1894ca5efda066b56969e5bfde392ea5c720afd2
MamboBryan <mambobryan@gmail.com>
2023-03-02 16:49:17 +0300

    :tada: inital commit

---
+++ blog/2023-03-01-newsletter/index.md
@@ -0,0 +1,48 @@
+---
+slug: newsletter
+title: Newsletter 9
+authors: [mambo, sheliza]
+tags: [navigation, di, compose, test]
+---
+Hola👋 gritty engineers! As we've come to start of another month, we would like to bring you all up to speed with things that have been happening around our amazing android community.
+
+Starting us off is…
+
+## 1. Compose Camp Things.
+After many years of imperative UI using Java and XML to render views on a screen, `compose` came to make it easier and simpler. Its not only declarative, but also saves time while creating the said components.
+
+In February KotlinKenya, Android254 and WomenTechMakers Nairobi sought to focus more on UI frameworks by organizing interactive and helpful weekend sessions on `compose`. These sessions got the audience acquinted with common way of using `compose` and also the `Kotlin` language.
+
+:::tip
+
+There is an ongoing challange for the compose camp session. Click [here](https://twitter.com/254androiddevs/status/1630511775283044352?t=cbyCED8GjQIWkpBD5Yurng&s=19) to find out more!
+
+:::
+
+## 2. DroidConKE Android App.
+As we eagerly anticipate this year’s Droidcon, nothing gets us more fired up as collaboratively developing the app. This year the team has decided to open source the app early in advance so that anybody can contribute!.
+
+The app is a treasure trove even for the most seasoned developers. From excellent modularization to integrated testing. There are a number of issues added and we would love to see you add yours as well! To do so, head on over [here](https://github.com/droidconKE/droidconKE2023Android/blob/main/CONTRIBUTING.md) to contribute.
+
+## 3. KotlinKenya/Android254 call for speakers.
+As a community, we usually host monthly meet up sessions which we would be delighted to have you guys apply as speakers.If you are wondering who is legible to do so, it’s anyone who considers themselves as either; a beginner, intermediate or expert.
+If you fit into any of the categories, head on over [here](https://docs.google.com/forms/d/1BeuuzLsVg0zUf52umbYNsZPPZ7Tfp1KHPYCee22rics/viewform?edit_requested=true) to apply. We are looking forward to receive them.
+
+## 4. Featured Section (NEW) :fire: :fire:
+There are tons of content around the internet for Android and Kotlin. This year, we’ve added this new section to shine light upon a list of interesting; articles, sessions, libraries and videos we’ve come across in our Kenyan community.
+
+
+|ARTICLES|
+|:-------|
+|[Dependency Injection with Hilt](https://github.com) by Victor Irungu.|
+|In-App purchase In Jepack Compose by Felix Kariuki.|
+
+|LIBRARIES|
+|:------|
+|Daraja Multiplatform by Victor Kabata[link]|
+|Signature Pad by Joel Kanyi[link]|
+
+|VIDEOS|
+|:------|
+|Unit testing navigation in Jetpack compose by Breens Mbaka[link]|
+|How to make a cross-platform movie app by using Kotlin Multiplatform[link]|
---
+++ docusaurus.config.js
@@ -0,0 +1,145 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "KotlinKenya",
+  tagline: "Latest KotlinKenya and Android254 community updates",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-test-site.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "MamboBryan", // Usually your GitHub org/user name.
+  projectName: "newsletter", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "KotlinKenya",
+        logo: {
+          alt: "Kotlin Community Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          { to: "/blog", label: "Newsletter", position: "left" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "KotlinKenya",
+            items: [
+              {
+                label: "Telegram",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Meetup",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "Android254",
+            items: [
+              {
+                label: "Telegram",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Meetup",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "Github",
+            items: [
+              {
+                label: "KotlinKenya",
+                href: "https://github.com/facebook/docusaurus",
+              },
+              {
+                label: "Android254",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "DroidconKe",
+                href: "https://droidcon.co.ke/s",
+              },
+              {
+                label: "Droid Pwani",
+                href : "https://twitter.com/DroidPwani_KE"
+              }
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} KotlinKenya. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
